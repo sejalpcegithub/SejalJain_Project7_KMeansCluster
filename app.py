58a6edcd5b7cb80936a1eb37a40f268cdaf1dfda
sejalpcegithub <101696920+sejalpcegithub@users.noreply.github.com>
2022-08-09 08:46:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 08:41:17 2022
+
+@author: Dell
+"""
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('cluster.pkl','rb')) 
+
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+   
+    exp1 = int(request.args.get('exp1'))
+    exp2 = int(request.args.get('exp2'))
+    exp3 = int(request.args.get('exp3'))
+    exp4 = int(request.args.get('exp4'))
+
+    prediction = classifier.predict(sc.transform([[exp1,exp2,exp3,exp4]]))
+    
+    print("K-means prediction",prediction)
+    if prediction==[0]:
+      print("cluster 0")
+    elif prediction==[1]:
+      print("cluster 1")
+    else:
+      print("cluster 2")
+
+        
+    return render_template('index.html', prediction_text='k-means cluster = {}'.format(prediction))
+
+
+app.run()
